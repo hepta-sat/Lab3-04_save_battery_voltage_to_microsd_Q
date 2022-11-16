c67f9ef53d31ce644dbb11a53dcd08850d4b8c11
Charlie.com <csru22005@g.nihon-u.ac.jp>
2022-11-16 01:06:13 +0000

    hepta

--- main.cpp
+++ main.cpp
@@ -1,12 +1,32 @@
 #include "mbed.h"
+#include "HEPTA_CDH.h"
+#include "HEPTA_EPS.h"
+ 
+RawSerial pc(USBTX,USBRX,9600);
 
-DigitalOut myled(LED1);
+ 
+ 
+int main()
+{
 
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+    float bt;
+    char str[100];
+    mkdir("/sd/mydir", 0777);
+    FILE *fp = fopen("/sd/mydir/test.txt","w");
+    if(fp == NULL) {
+        error("Could not open file for write\r\n");
+    }
+    for(int i = 0; i < 10; i++) {
+       
+       
+       
+    }
+    fclose(fp);
+    fp = fopen("/sd/mydir/test.txt","r");
+    for(int j = 0; j < 10; j++) {
+        
+        
+    }
+    fclose(fp);
+    pc.printf("Goodbye!!\r\n");
 }
